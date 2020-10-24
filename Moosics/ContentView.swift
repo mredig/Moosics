1e8cb489cbc86f7dc15389c603851a8a5325e6a7
Michael Redig <mredig@gmail.com>
2020-10-24 15:00:53 -0500

    initial commit

---
+++ Moosics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moosics
+//
+//  Created by Michael Redig on 10/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moosics/MoosicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoosicsApp.swift
+//  Moosics
+//
+//  Created by Michael Redig on 10/24/20.
+//
+
+import SwiftUI
+
+@main
+struct MoosicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
